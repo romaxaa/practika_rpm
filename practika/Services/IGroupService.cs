28a069db1716193f0b77e76423bf23569abf7f92
Roman <romkisner@gmail.com>
2026-01-13 17:34:34 +0700

    Добавление файла IGroupService

---
+++ practika/Services/IGroupService.cs
@@ -0,0 +1,10 @@
+﻿using practika.Data; // если нужны модели базы
+using practika.DTO;  // если используешь DTO
+
+namespace practika.Services
+{
+    public interface IGroupService
+    {
+        Task<List<GroupDto>> GetAllGroupsAsync();
+    }
+}
