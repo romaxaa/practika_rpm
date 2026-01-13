8f93a5400d684046acca5f2b0c907abcb10d3ecd
Roman <romkisner@gmail.com>
2026-01-13 17:32:37 +0700

    Добавление файла GroupDto

---
+++ practika/DTO/GroupDto.cs
@@ -0,0 +1,8 @@
+﻿namespace practika.DTO
+{
+    public class GroupDto
+    {
+        public int Id { get; set; }     // ID группы
+        public string Name { get; set; } // Название группы
+    }
+}
