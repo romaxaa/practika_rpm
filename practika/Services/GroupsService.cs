a72b59efffa74e6b800ede590f05358a7b9cef6a
Roman <romkisner@gmail.com>
2026-01-13 17:33:27 +0700

    Добавление файла GroupsService

---
+++ practika/Services/GroupsService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using practika.Data;
+using practika.DTO;
+
+namespace practika.Services
+{
+    // Класс реализует интерфейс IGroupService
+    public class GroupsService : IGroupService
+    {
+        private readonly AppDbContext _context;
+
+        public GroupsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Получение всех групп
+        public async Task<List<GroupDto>> GetAllGroupsAsync()
+        {
+            return await _context.StudentGroups
+                .Select(g => new GroupDto
+                {
+                    Id = g.GroupId,      // используем реальные свойства из StudentGroup
+                    Name = g.GroupName
+                })
+                .ToListAsync();
+        }
+    }
+}
