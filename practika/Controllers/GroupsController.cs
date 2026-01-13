a8f9db86618ad9999156f9f0a2f1e5384f25a0e0
Roman <romkisner@gmail.com>
2026-01-13 17:31:42 +0700

    Добавление файла GroupsConroller

---
+++ practika/Controllers/GroupsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practika.Services;
+using practika.DTO;
+
+namespace practika.Controllers
+{
+    [ApiController]
+    [Route("api/groups")]
+    public class GroupsController : ControllerBase
+    {
+        private readonly IGroupService _groupService;
+
+        public GroupsController(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllGroups()
+        {
+            var groups = await _groupService.GetAllGroupsAsync();
+            return Ok(groups);
+        }
+    }
+}
