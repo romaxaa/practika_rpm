60c75bab9680ddf7e2e28f962dbe1e5f332f55a4
Roman <romkisner@gmail.com>
2026-01-13 17:29:25 +0700

    Добавление кода эндпоинта для получения списка всех групп

--- practika/Data/AppDbContext.cs
+++ practika/Data/AppDbContext.cs
@@ -1,6 +1,7 @@
-﻿using practika.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using practika.Models;
 using practika.Models.CollegeSchedule.Models;
+
 namespace practika.Data
 {
     public class AppDbContext : DbContext
@@ -8,35 +9,49 @@ namespace practika.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        // Таблицы базы данных
         public DbSet<Building> Buildings => Set<Building>();
         public DbSet<Classroom> Classrooms => Set<Classroom>();
         public DbSet<Teacher> Teachers => Set<Teacher>();
         public DbSet<Subject> Subjects => Set<Subject>();
         public DbSet<Specialty> Specialties => Set<Specialty>();
+
+        // Таблица групп студентов (раньше "Groups")
         public DbSet<StudentGroup> StudentGroups => Set<StudentGroup>();
+
         public DbSet<Weekday> Weekdays => Set<Weekday>();
         public DbSet<LessonTime> LessonTimes => Set<LessonTime>();
         public DbSet<Schedule> Schedules => Set<Schedule>();
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // Индексы для Schedule
+
+            // Индексы для Schedule (уникальность)
             modelBuilder.Entity<Schedule>()
-            .HasIndex(s => new
-            {
-                s.LessonDate,
-                s.LessonTimeId,
-                s.GroupId,
-                s.GroupPart
-            })
-            .IsUnique();
+                .HasIndex(s => new
+                {
+                    s.LessonDate,
+                    s.LessonTimeId,
+                    s.GroupId,
+                    s.GroupPart
+                })
+                .IsUnique();
+
             modelBuilder.Entity<Schedule>()
-            .HasIndex(s => new { s.LessonDate, s.LessonTimeId, s.ClassroomId })
-            .IsUnique();
-            // Конвертация enum в string
+                .HasIndex(s => new
+                {
+                    s.LessonDate,
+                    s.LessonTimeId,
+                    s.ClassroomId
+                })
+                .IsUnique();
+
+            // Конвертация enum GroupPart в строку
             modelBuilder.Entity<Schedule>()
-            .Property(s => s.GroupPart)
-            .HasConversion<string>();
+                .Property(s => s.GroupPart)
+                .HasConversion<string>();
         }
     }
 }
