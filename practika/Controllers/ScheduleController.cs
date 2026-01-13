e23d0d601bde758bda4658bb116048e07102e26c
Roman <romkisner@gmail.com>
2026-01-13 14:55:58 +0700

    Добавлен api ScheduleController

---
+++ practika/Controllers/ScheduleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using practika.Data;
+using practika.Services;
+
+namespace practika.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+            return Ok(result);
+        }
+    }
+}
